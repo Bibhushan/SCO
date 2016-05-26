7527b573fc63f94d05818562c4afb5df58c87626
Bibhushan <bibhushan.iitd@gmail.com>
2016-05-26 16:33:34 +0530

    1. changed getKeyFields.R to return primary keys, foreign keys or both. 2. allowed some flexibility in naming the key fields.

--- getKeyFields.R
+++ getKeyFields.R
@@ -1,7 +1,19 @@
 # Just a simple function which returns the key fields from a list of fields 
 
-getKeyFields <- function(fieldNames, fieldTypes){
+getKeyFields <- function(fieldNames, fieldTypes, onlyPK = F, onlyFK = F){
   
-  return(fieldNames[fieldTypes %in% c('PK', 'FK')])
+  # Try to parse the field type based on the first two characters of the fieldType
+  
+  fTypes <- tolower(substr(fieldTypes, 1, 2))
+    
+  PKs <- fieldNames[fTypes %in% c('pk', 'pr')]
+    
+  if (onlyPK) return(PKs) 
+  
+  FKs <- fieldNames[fTypes %in% c('fk', 'fo', 'fr')]
+  
+  if (onlyFK) return(FKs)
+  
+  return(merge(PKs, FKs))
   
 }
